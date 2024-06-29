3171e8e76d3212538b59a8421c20f2e4d16760ab
patchker <paczker@icloud.com>
2024-06-29 19:04:47 +0200

    Initial commit

---
+++ src/main/java/org/example/utms/UtmsApplication.java
@@ -0,0 +1,13 @@
+package org.example.utms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtmsApplication.class, args);
+	}
+
+}
